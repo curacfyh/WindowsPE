160ea559a7942e5271914cb42e3202d25a023dc2
curacfyh <curacfyh@gmail.com>
2023-08-10 17:28:59 +0800

    1

--- main.c
+++ main.c
@@ -19,6 +19,12 @@ const struct _IMAGE_FILE_HEADER *gVirtualImageFileHeader;
 const struct _IMAGE_OPTIONAL_HEADER *gVirtualImageOptionalHeader;
 const struct _IMAGE_SECTION_HEADER *gVirtualImageSectionHeader;
 
+struct _IMAGE_DOS_HEADER *gNewDosHeader;
+struct _IMAGE_NT_HEADERS *gNewNtHeaders;
+struct _IMAGE_FILE_HEADER *gNewFileHeader;
+struct _IMAGE_OPTIONAL_HEADER *gNewOptionalHeader;
+struct _IMAGE_SECTION_HEADER *gNewSectionHeader;
+
 void initialFileHeader() {
     gFileImageDosHeader = (struct _IMAGE_DOS_HEADER *) fileBuffer;
     gFileImageNtHeaders = (struct _IMAGE_NT_HEADERS *) (fileBuffer + gFileImageDosHeader->e_lfanew);
@@ -104,6 +110,60 @@ void initialVirtualHeader() {
                                                                    gVirtualImageFileHeader->SizeOfOptionalHeader);
 }
 
+void initialNewBufferHeader() {
+    gNewDosHeader = (struct _IMAGE_DOS_HEADER *) newBuffer;
+    gNewNtHeaders = (struct _IMAGE_NT_HEADERS *) (newBuffer + gNewDosHeader->e_lfanew);
+    gNewFileHeader = &(gNewNtHeaders->FileHeader);
+    gNewOptionalHeader = &(gNewNtHeaders->OptionalHeader);
+    gNewSectionHeader = (struct _IMAGE_SECTION_HEADER *) ((BYTE *) gNewOptionalHeader +
+                                                          gNewFileHeader->SizeOfOptionalHeader);
+}
+
+void implantCode() {
+    //准备要植入的代码 6A 00 6A 00 6A 00 6A 00 E8 XX XX XX XX E9 XX XX XX XX，共18字节，push call jmp
+    BYTE codePush[] = {0x6A, 0x00, 0x6A, 0x00, 0x6A, 0x00, 0x6A, 0x00};
+    BYTE codeE8 = 0xE8;
+    BYTE codeE9 = 0xE9;
+    //先找空白区，这里使用第一个节区对齐后的空白区，然后看空白区是否够放代码
+    DWORD blankSize = gNewSectionHeader->SizeOfRawData - gNewSectionHeader->VirtualAddress;
+    if (blankSize < 18) {
+        printf("空白区不够植入代码。\n");
+        exit(1);
+    }
+    //找出空白区首偏移地址
+    DWORD blankFileOffset = gNewSectionHeader->PointerToRawData + gNewSectionHeader->Misc.VirtualSize;
+    DWORD blankVirtualOffset = gNewSectionHeader->VirtualAddress + gNewSectionHeader->Misc.VirtualSize;
+    //计算E8代码的偏移量
+    //messageBoxA的地址 0x77530C10，这是绝对地址，所以参与计算的其它地址也用绝对地址，注意这个地址可能会变
+    //E8指令之后的地址=blankVirtualOffset+8+5+imageBase
+    DWORD E8AppendOffset = 0x77530C10 - (blankVirtualOffset + 8 + 5 + 0x00400000);
+    printf("E8 offset: 0x%08x\n", E8AppendOffset);
+    //计算E9代码的偏移量，即原来的EOP 0x000E1D80，这是个文件地址偏移量，注意这个地址可能会变
+    DWORD E9AppendOffset = 0x000E1D80 - (blankVirtualOffset + 18);
+    printf("E9 offset: 0x%08x\n", E9AppendOffset);
+    //植入代码
+    memcpy(newBuffer + blankFileOffset, codePush, 8);
+    memcpy(newBuffer + blankFileOffset + 8, &codeE8, 1);
+    memcpy(newBuffer + blankFileOffset + 9, &E8AppendOffset, 4);
+    memcpy(newBuffer + blankFileOffset + 13, &codeE9, 1);
+    memcpy(newBuffer + blankFileOffset + 14, &E9AppendOffset, 4);
+    //修改OEP为植入代码的位置，在文件偏移地址0x128位置
+    memcpy(newBuffer + 0x128, &blankVirtualOffset, 4);
+}
+
+void writeFile(DWORD newBufferSize) {
+    FILE *outFile = fopen("C:\\Users\\Chou\\Desktop\\Windows On Top_new.exe", "wb");
+    if (outFile == NULL) {
+        printf("Failed to open file: %s\n", outFile);
+        exit(1);
+    }
+    size_t writeCount = fwrite(newBuffer, newBufferSize, 1, outFile);
+    if (writeCount != 1) {
+        printf("Failed to write file.\n");
+    }
+    fclose(outFile);
+}
+
 //newBuffer为新的fileBuffer
 void imageBufferToNewBuffer() {
     initialVirtualHeader();
@@ -129,16 +189,9 @@ void imageBufferToNewBuffer() {
         BYTE *pNewBufferSection = (BYTE *) newBuffer + curSectionHeader->PointerToRawData;
         memcpy(pNewBufferSection, pImageSection, curSectionHeader->SizeOfRawData);
     }
-    FILE *outFile = fopen("C:\\Users\\Chou\\Desktop\\IPMSG2007_new.exe", "wb");
-    if (outFile == NULL) {
-        printf("Failed to open file: %s\n", outFile);
-        exit(1);
-    }
-    size_t writeCount = fwrite(newBuffer, newBufferSize, 1, outFile);
-    if (writeCount != 1) {
-        printf("Failed to write file.\n");
-    }
-    fclose(outFile);
+    initialNewBufferHeader();
+    implantCode();
+    writeFile(newBufferSize);
 }
 
 //节区中虚拟内存偏移地址转文件偏移地址，不考虑VirtualSize比SizeOfRawData大的情况
@@ -165,7 +218,7 @@ DWORD RVAToFOA(const BYTE *pVirtualAddress) {
 }
 
 int main() {
-    readFile("C:\\Users\\Chou\\Desktop\\IPMSG2007.exe");
+    readFile("C:\\Users\\Chou\\Desktop\\Windows On Top.exe");
     if (fileBuffer == NULL) {
         exit(1); // 文件读取失败，退出程序
     }
